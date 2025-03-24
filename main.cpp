fc9ba6621355729200616e6a3b29163d190aa491
Kirill <Kirill_0901@mail.ru>
2025-03-24 11:40:58 +0300

    Own recursive directory traversal, which adds cycles check not only with symlinks.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <regex>
+#include <vector>
 #include <filesystem>
 
 using namespace std::filesystem;
@@ -11,28 +12,47 @@ int main(int argc, char **argv){
 
     std::string filter = "^(.)*\.cpp$";
 
+    std::vector<std::pair<path, int>> path_stack = {{target_directory, 0}};
+    std::unordered_map<std::string, bool> visited;
+
     auto pattern = std::regex(filter);
 
+    if (!exists(target_directory) 
+        || (!is_directory(target_directory) && !is_block_file(target_directory))){
+        std::cout << "Directory not found." << "\n"; 
+        return 0;
+    }
 
-    if (exists(target_directory) 
-        && is_directory(target_directory)) { 
-        for (auto file_ptr = recursive_directory_iterator(target_directory);
-        file_ptr != recursive_directory_iterator(); ++file_ptr) {
-            
-            path entry = *file_ptr;
+    while (!path_stack.empty()){
+        path cur_dir = path_stack.back().first;
+        int cur_depth = path_stack.back().second;
+        path_stack.pop_back();
 
-            if (file_ptr.depth() == depth && is_directory(entry)){
-                file_ptr.disable_recursion_pending();
+        if (cur_depth == depth + 1){
+            continue;
+        }
+
+        std::string path_to_cur_dir = "";
+
+        for (auto entry: directory_iterator(cur_dir)) {
+            path_to_cur_dir = entry.path().parent_path().string();
+
+            if (visited[path_to_cur_dir]){
+                break;
+            }
+
+            if (is_directory(entry)){
+                path_stack.push_back({entry, cur_depth + 1});
                 continue;
             }
 
-            if (is_regular_file(entry) && std::regex_match(std::string(entry.filename()), pattern)){
+            if (is_regular_file(entry) && std::regex_match(entry.path().filename().string(), pattern)){
                 std::cout << "file: " << entry << "\n" <<
                 "size: " << file_size(entry) << " bytes" << "\n\n";
             }
-        } 
-    } 
-    else { 
-        std::cout << "Directory not found." << "\n"; 
+        }
+
+        visited[path_to_cur_dir] = true;
     }
+
 }
